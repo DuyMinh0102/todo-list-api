cb587b608465f25a8298e67027cb564462085106
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-09-10 21:10:51 +0700

    Implemented dynamic display of tasks on homepage, next will be working on marking completion, deleting tasks, and logging out user.

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,9 @@ const modal = document.querySelector("#addTaskModal");
 const openBtn = document.querySelector("#openAddTaskBtn");
 const closeBtn = document.querySelector("#closeModalBtn");
 const form = document.querySelector("#addTaskForm");
-const formError = document.querySelector("#form-error");
+const formError = document.querySelector("#task-from-error");
+const taskGrid = document.querySelector("#task-grid");
+const taskTemplate = document.querySelector("#task-card-template");
 
 function changeContent(targetElement, content) {
   if (targetElement) targetElement.textContent = content;
@@ -10,6 +12,46 @@ function changeContent(targetElement, content) {
 
 openBtn.addEventListener("click", () => modal.showModal());
 closeBtn.addEventListener("click", () => modal.close());
+
+async function loadTasks() {
+  try {
+    const response = await fetch("/tasks");
+
+    if (response.status === 401) {
+      window.location.href = "/login";
+      return;
+    }
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch tasks from server.");
+    }
+
+    const tasks = await response.json();
+    renderTasks(tasks);
+  } catch (err) {
+    console.error("Error loading tasks: ", err);
+    if (taskGrid) {
+      taskGrid.innerHTML = "<p class = 'error-text'>Unable to load tasks.</p>";
+    }
+  }
+}
+
+function renderTasks(tasks) {
+  taskGrid.innerHTML = "";
+
+  if (tasks.length === 0) {
+    taskGrid.innerHTML = "<p>No tasks yet. Click '+ Add task' to get started. </p>";
+  }
+
+  tasks.forEach((task) => {
+    const clone = taskTemplate.content.cloneNode(true);
+
+    clone.querySelector(".task-title").textContent = task.title;
+    clone.querySelector(".task-description").textContent = task.description || "No description provided";
+
+    taskGrid.appendChild(clone);
+  });
+}
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
@@ -38,9 +80,13 @@ form.addEventListener("submit", async (event) => {
       return;
     }
 
+    form.reset();
     modal.close();
+    await loadTasks();
   } catch (error) {
     console.error(error.message);
     changeContent(formError, "Unable to connect to server.");
   }
 });
+
+loadTasks();
