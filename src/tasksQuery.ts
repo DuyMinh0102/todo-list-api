53d987b3577360808aa769757b13a70af73c5196
DuyMinh0102 <minhnguyenduy1234@gmail.com>
2026-09-12 20:52:21 +0700

    Tasks can be now edited.

--- src/tasksQuery.ts
+++ src/tasksQuery.ts
@@ -1,6 +1,6 @@
 import { ServerResponse, IncomingMessage } from "node:http";
 import { MAX_BODY_SIZE, parseCookie, User } from "./helpers";
-import { getSessionInfo, insertTaskData, getTasks, deleteTask, markAsComplete } from "./database";
+import { getSessionInfo, insertTaskData, getTasks, deleteTask, markAsComplete, updateTask } from "./database";
 
 export async function handleAddTaskQuery(req: IncomingMessage, res: ServerResponse<IncomingMessage>): Promise<void> {
   let body: Buffer[] = [];
@@ -92,17 +92,17 @@ export function handleGetTasksQuery(req: IncomingMessage, res: ServerResponse<In
   }
 }
 
-export function handleDeleteTaskQuery(
+function checkSessionValidity(
   req: IncomingMessage,
   res: ServerResponse<IncomingMessage>,
   taskIDStr: string | undefined,
-) {
+): boolean {
   const taskID = Number(taskIDStr);
 
   if (!taskID || isNaN(taskID)) {
     res.writeHead(400, { "Content-Type": "text/plain" });
     res.end("Valid task ID is required in the URL.");
-    return;
+    return false;
   }
 
   const cookies = parseCookie(req.headers.cookie);
@@ -111,8 +111,22 @@ export function handleDeleteTaskQuery(
   if (!sessionID) {
     res.writeHead(401, { "Content-Type": "text/plain" });
     res.end("Unauthorized.");
-    return;
+    return false;
   }
+
+  return true;
+}
+
+export function handleDeleteTaskQuery(
+  req: IncomingMessage,
+  res: ServerResponse<IncomingMessage>,
+  taskIDStr: string | undefined,
+) {
+  if (!checkSessionValidity(req, res, taskIDStr)) return;
+
+  const taskID = Number(taskIDStr);
+  const cookies = parseCookie(req.headers.cookie);
+  const sessionID = cookies["session_id"];
 
   try {
     const currentUser = getSessionInfo.get(sessionID) as User | undefined;
@@ -145,22 +159,11 @@ export function markTaskAsDone(
   res: ServerResponse<IncomingMessage>,
   taskIDStr: string | undefined,
 ) {
+  if (!checkSessionValidity(req, res, taskIDStr)) return;
+
   const taskID = Number(taskIDStr);
-
-  if (!taskID || isNaN(taskID)) {
-    res.writeHead(400, { "Content-Type": "text/plain" });
-    res.end("Valid task ID is required in the URL.");
-    return;
-  }
-
   const cookies = parseCookie(req.headers.cookie);
   const sessionID = cookies["session_id"];
-
-  if (!sessionID) {
-    res.writeHead(401, { "Content-Type": "text/plain" });
-    res.end("Unauthorized.");
-    return;
-  }
 
   try {
     const currentUser = getSessionInfo.get(sessionID) as User | undefined;
@@ -186,4 +189,71 @@ export function markTaskAsDone(
     res.writeHead(500, { "Content-Type": "text/plain" });
     res.end("Internal Server Error");
   }
+}
+
+export async function editTaskDescription(
+  req: IncomingMessage,
+  res: ServerResponse<IncomingMessage>,
+  taskIDStr: string | undefined,
+) {
+  let body: Buffer[] = [];
+  let bodySize = 0;
+
+  req
+    .on("error", (err) => {
+      console.error(err);
+
+      if (!res.headersSent) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end("Bad request");
+      }
+    })
+    .on("data", (chunk) => {
+      bodySize += chunk.length;
+
+      if (bodySize > MAX_BODY_SIZE) {
+        res.writeHead(413, { "Content-Type": "text/plain" });
+        res.end("Payload too large");
+        req.destroy();
+      }
+
+      body.push(chunk);
+    })
+    .on("end", async () => {
+      if (res.writableEnded) return;
+      const parsedBody = new URLSearchParams(Buffer.concat(body).toString());
+
+      if (!checkSessionValidity(req, res, taskIDStr)) return;
+
+      const taskID = Number(taskIDStr);
+      const cookies = parseCookie(req.headers.cookie);
+      const sessionID = cookies["session_id"];
+
+      try {
+        const currentUser = getSessionInfo.get(sessionID) as User | undefined;
+
+        if (!currentUser) {
+          res.writeHead(401, { "Content-Type": "text/plain" });
+          res.end("Unauthorized: Invalid session");
+          return;
+        }
+
+        const newDesc = parsedBody.get("description");
+
+        const info = updateTask.run(newDesc, taskID, currentUser.id);
+
+        if (info.changes === 0) {
+          res.writeHead(404, { "Content-Type": "text/plain" });
+          res.end("Task not found or you do not have permission to mark it");
+          return;
+        }
+
+        res.writeHead(200, { "Content-Type": "text/plain" });
+        res.end(`Task's description updated successfully. ID: ${taskID}`);
+      } catch (err) {
+        console.error("Update task error: ", err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      }
+    });
 }
